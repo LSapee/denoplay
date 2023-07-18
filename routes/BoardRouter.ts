b01a75f8bf4a326306752dc4c1e40e288d4a9772
LSapee <dlworhks94@gmail.com>
2023-07-18 16:43:07 +0900

    deno KV가지고 놀기

--- routes/BoardRouter.ts
+++ routes/BoardRouter.ts
@@ -4,17 +4,19 @@ const router = new Router();
 interface Myboard {
     title: string;
     post : string;
+    uuid : string;
 }
 
 
 const kv:Deno.Kv = await Deno.openKv();
 // await kv.set(key,{title:"감자",post:"감자1"});
-
+await kv.delete(["boards"]);
 router
     .get("/board",async (ctx):Promise<void> =>{
-        const dataList = [];
+        const dataList= [];
         const result  = await kv.list({prefix:["boards"]});
         for await (const a of result){
+            // kv.delete(a.key);
             dataList.push(a.value);
         }
         ctx.response.body = dataList;
@@ -24,14 +26,28 @@ router
 
         const {value} = await ctx.request.body({type:"json"});
         const {title,post} = await value;
-        const key:string[] = ["boards",crypto.randomUUID()];
-        const boardData:Myboard  = {title:title,post:post};
+        const uuid = crypto.randomUUID();
+        const key:string[] = ["boards",uuid];
+        const boardData:Myboard  = {title:title,post:post,uuid:uuid};
         await kv.set(key,boardData);
         ctx.response.status = 200;
         ctx.response.body = {data : "글쓰기 성공"};
     });
 router
-    .delete("board",(ctx)=>{
+    .delete("/board/:id",async (ctx)=>{
+        const {id} = ctx.params;
+        await kv.delete(["boards",id]);
+        ctx.response.status = 200;
+        ctx.response.body = {data : "글 삭제 완료"};
+    });
 
+router
+    .get("/board/:id",async (ctx):Promise<void>=>{
+        const {id} = ctx.params;
+        const result = await kv.get(["boards",id]);
+        // unknown타입이 아니라 Myboard 타입이라고 명시 해줌
+        const post:Myboard = result.value as Myboard;
+        ctx.response.status =200;
+        ctx.response.body = post;
     });
 export default new Router().use("/api",router.routes(),router.allowedMethods());
---
+++ test.js
@@ -0,0 +1,14 @@
+function a(){
+    setInterval(at,1000);
+}
+
+function at(){
+    for(let i=0; i<10; i++) console.log(i);
+}
+
+function ad(){
+    for(let i=0; i<10; i++) console.log("kkk");
+}
+
+a();
+ad();
